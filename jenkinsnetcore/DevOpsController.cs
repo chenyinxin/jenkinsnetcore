1f30ad73d3ffe5db04fe474caf57f640b8472e76
chenyinxin <chenyinxin@163.com>
2018-07-24 11:36:30 +0800

    20180724

---
+++ jenkinsnetcore/DevOpsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace jenkinsnetcore
+{
+    public class DevOpsController: Controller
+    {
+        [Route("/GetString")]
+        public string GetString()
+        {
+            return "NetCore结果：" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+        [Route("/GetJava")]
+        public string GetJava()
+        {
+            WebClient wc = new WebClient();
+            string result = wc.DownloadString("http://java/GetString");
+            return "K8s调用（http://java/GetString）结果：" + result;
+        }
+    }
+}
